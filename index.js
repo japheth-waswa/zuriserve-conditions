bab726a5169d3990d870dab0c30b7f60ae42bc71
japheth-waswa <japhethwaswa@gmail.com>
2022-12-07 13:41:37 +0300

    added 2 new methods

---
+++ index.js
@@ -0,0 +1,34 @@
+/**
+ * TYPES OF CONDITIONS
+ * 1. object conditions
+ * 2. fee conditions
+ * 3. end result conditions
+ * todo `conditional result` can be gotten for specified objects in list ie [object1,object2] or if an empty list [] then compute all conditional results for all objects
+ *
+ */
+
+export const computeConditionalResult = ({
+  objectLabelIds = [],
+  objectLabelData = {},
+  serviceConditions = [],
+  channels = [],
+}) => {
+  try {
+    return 'ready to start processing here.';
+  } catch (e) {
+    return false;
+  }
+};
+
+export const testingAgain = ({
+  objectLabelIds = [],
+  objectLabelData = {},
+  serviceConditions = [],
+  channels = [],
+}) => {
+  try {
+    return 'peter pan';
+  } catch (e) {
+    return false;
+  }
+};
