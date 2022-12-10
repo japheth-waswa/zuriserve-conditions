1a9a1c6f325ee68f93cab5b8779e2b5344d4f52a
japheth-waswa <japhethwaswa@gmail.com>
2022-12-10 11:51:14 +0300

    object conditions beta-stage done

--- index.js
+++ index.js
@@ -3,30 +3,48 @@ import { processTransactionData } from "./lib/transaction.data.js";
 //this file is just an interface to specific methods in lib
 /**
  * TYPES OF CONDITIONS
- * 1. object conditions
+ * 1. object conditions (/\)
  * 2. fee conditions
  * 3. end result conditions
  * todo `conditional result` can be gotten for specified objects in list ie [object1,object2] or if an empty list [] then compute all conditional results for all objects
  *
  */
-// export const computeObjectConditionalResult = ({
-//   objectLabelIds = [],
-//   objectLabelData = {},
-//   serviceConditions = [],
-//   channels = [],
-// }) => {
-//   try {
-//     return processObjectConditionalResult({
-//       objectLabelIds,
-//       objectLabelData,
-//       serviceConditions,
-//       channels,
-//     });
-//   } catch (e) {
-//     return false;
-//   }
-// };
 
+/**
+ * 
+ * @param objectLabelsData :- Parsed transaction data that this method can interpret. Use the method `parseTransactionData` to get a valid data format.
+ * @param objectLabelConditions :- {
+    objectLabel1: { conditionId1: {objectId,affects,conditionalStatements,objectActions} },
+    objectLabel2: { conditionId3: {objectId,affects,conditionalStatements,objectActions} },
+  }
+  @param triggerObjectIds :- specifiy object ids whose effect you wish to determine. Basically which effects do the specified object(s) trigger.
+  @removeDuplicates :- Whether to remove duplicate actionTypes in an affected object(s) or step(s).
+ * @returns {effects: { objects: {}, steps: {} }} :- Always check if `objects` or `steps` is set before using them in your code.
+ */
+export const parseObjectConditionalResult = ({
+  objectLabelsData = {},
+  objectLabelConditions = {},
+  triggerObjectIds = [],
+  removeDuplicates = true,
+}) => {
+  try {
+    return processObjectConditionalResult({
+      objectLabelsData,
+      objectLabelConditions,
+      triggerObjectIds,
+      removeDuplicates,
+    });
+  } catch (e) {
+    return false;
+  }
+};
+
+/**
+ * Interface
+ * Parse data into a format that can be used in conditions
+ * @param data This is the transaction data
+ * @returns {objectLabelsData,steps}
+ */
 export const parseTransactionData = ({ data }) => {
   try {
     return processTransactionData({ data });
